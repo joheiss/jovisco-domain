0cb89db4c34ec3c1002b9b3bfcd1636f24b1f090
joheiss <josef.heiss@jovisco.de>
2019-02-07 10:21:29 +0100

    find vat percentage

--- lib/settings/vat.ts
+++ lib/settings/vat.ts
@@ -7,11 +7,9 @@ export class Vat {
 
     static findVatPercentage(vatSettings: SettingData, taxCode: string, issuedAt: Date): number {
 
-        const percentage = vatSettings.values
+       return vatSettings.values
             .find((vatSetting: VatData) => vatSetting.taxCode === taxCode && Vat.isValid(vatSetting, issuedAt))
             .percentage;
-        console.log('Vat percentage found: ', percentage);
-        return percentage;
     }
 
     private static isValid(vatSetting: VatData, issuedAt: Date): boolean {
